0fa662ceff1c0213cabadef82a449e63e38cd6d3
alumno <alumno@147.156.80.224>
2021-10-07 12:52:16 +0200

    MisReservasView implementado

--- CampingGas/src/campinggas/CampingGas.java
+++ CampingGas/src/campinggas/CampingGas.java
@@ -1,22 +1,19 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package campinggas;
 
 /**
  *
- * @author pablo, david, carlos, pedro y raul
+ * @author alumno
  */
 public class CampingGas {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        
+    public void main(String args[]){
         VentanaPrincipalView yoLoHagoEnCMasPlas = new VentanaPrincipalView();
+        /*********************************************************
+         * https://github.com/pabol97/Puente4.git
+         */
     }
-    
 }
